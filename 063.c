7345a42f61ee47cae626af2362ddb4e42178fba7
srivastava2008harshit-ux <srivastava2008harshit@gmail.com>
2026-03-08 18:05:40 +0530

    Add files via upload

---
+++ 063.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+void push(int val) {
+    stack[++top] = val;
+}
+
+void pop() {
+    if(top != -1)
+        top--;
+}
+
+int main() {
+
+    int n, m, i, x;
+
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &x);
+        push(x);
+    }
+
+    scanf("%d", &m);
+
+    for(i = 0; i < m; i++) {
+        pop();
+    }
+
+    for(i = top; i >= 0; i--) {
+        printf("%d ", stack[i]);
+    }
+
+    return 0;
+}
